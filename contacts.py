534b9752015d98e40a957e832ffc140877544ef3
jmdagamewiz <75661560+jmdagamewiz@users.noreply.github.com>
2021-01-18 15:43:23 +0800

    Finish all functionalities
    Added limit lengths, polished contact printing, and added if_exists method to Contact class to check if ID exists

--- contacts.py
+++ contacts.py
@@ -1,12 +1,13 @@
 import sqlite3
 
-# TODO: add length limit for name, phone number and email
-# TODO: display contacts using ljust method by using limit length
-# TODO: structure code properly (esp. Contact class)
-
 
 class Contact:
+
     database_file_name = "contacts.db"
+    name_limit_length = 30
+    phone_limit_length = 15
+    email_limit_length = 30
+    rowid_limit_length = 3
 
     def __init__(self, name, phone_num, email):
         self.name = name
@@ -33,6 +34,21 @@ class Contact:
         connect.close()
 
     @classmethod
+    def if_exists(cls, rowid):
+        connect = sqlite3.connect(Contact.database_file_name)
+        cursor = connect.cursor()
+
+        cursor.execute(f"SELECT * from contacts WHERE rowid={rowid}")
+        contact = cursor.fetchall()
+
+        connect.close()
+
+        if len(contact) == 0:
+            return False
+        else:
+            return True
+
+    @classmethod
     def delete_from_database(cls, rowid):
         connect = sqlite3.connect(Contact.database_file_name)
         cursor = connect.cursor()
@@ -49,17 +65,6 @@ class Contact:
         cursor.execute(f"UPDATE contacts SET {info_type}='{value}' WHERE rowid={rowid}")
         connect.commit()
         connect.close()
-
-    @staticmethod
-    def has_table(database):
-        connect = sqlite3.connect(database)
-        cursor = connect.cursor()
-        cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='contacts'")
-
-        result = cursor.fetchone()[0]
-        connect.close()
-
-        return result
 
     @classmethod
     def create_database(cls):
@@ -80,8 +85,8 @@ class Contact:
         connect.commit()
         connect.close()
 
-    @staticmethod
-    def print():
+    @classmethod
+    def print(cls):
         connect = sqlite3.connect(Contact.database_file_name)
         cursor = connect.cursor()
 
@@ -89,21 +94,53 @@ class Contact:
             cursor.execute("SELECT rowid, * FROM contacts")
             contacts = cursor.fetchall()
 
-            # TODO: display contacts properly
+            print()
+            print("ID".ljust(cls.rowid_limit_length) + " " + "Name".ljust(cls.name_limit_length) + " "
+                  + "Phone #".ljust(cls.phone_limit_length) + " " + "Email".ljust(cls.email_limit_length))
+
             for contact in contacts:
-                print(contact)
+                print(str(contact[0]).ljust(cls.rowid_limit_length) + " " + contact[1].ljust(cls.name_limit_length) +
+                      " " + contact[2].ljust(cls.phone_limit_length) + " " + contact[3].ljust(cls.email_limit_length))
 
         except sqlite3.OperationalError as e:
             print("Operational Error: ", e)
         connect.close()
 
+    @staticmethod
+    def has_table(database):
+        connect = sqlite3.connect(database)
+        cursor = connect.cursor()
+        cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='contacts'")
+
+        result = cursor.fetchone()[0]
+        connect.close()
+
+        return result
+
 
 def get_new_contact():
     print("\nAdd Info for New Contact:")
     print()
-    name = input("Name: ")
-    phone_num = input("Phone Number: ")
-    email = input("Email: ")
+
+    while True:
+        name = input("Name: ")
+        if len(name) > Contact.name_limit_length:
+            print(f"Name must be less than {Contact.name_limit_length} characters.")
+        else:
+            break
+
+    while True:
+        phone_num = input("Phone Number: ")
+        if len(phone_num) > Contact.phone_limit_length:
+            print(f"Phone number must be less than {Contact.phone_limit_length} characters.")
+        else:
+            break
+    while True:
+        email = input("Email: ")
+        if len(email) > Contact.email_limit_length:
+            print(f"Phone number must be less than {Contact.email_limit_length} characters.")
+        else:
+            break
 
     contact1 = Contact(name, phone_num, email)
 
@@ -111,26 +148,42 @@ def get_new_contact():
 
 
 def update_contact_info(rowid):
-    # TODO: check if contact exists first
 
-    print(f"\nUpdate Info for Contact {rowid}: \n")
+    # checks if contact exists first
+    if Contact.if_exists(rowid):
 
-    while True:
-        info_type = input("What do you want to update? ")
-        if info_type in ["name", "phone", "email"]:
-            break
-        else:
-            print("Info type must be name, phone, or email only.")
+        # gets update info from user
+        print(f"\nUpdate Info for Contact {rowid}: \n")
 
-    value = input(f"{info_type} : ")
-    Contact.update_database(rowid, info_type, value)
+        while True:
+            info_type = input("What do you want to update? ")
+            if info_type in ["name", "phone_num", "email"]:
+                break
+            else:
+                print("Info type must be name, phone, or email only.")
 
-    print(f"\nSuccessfully updated Contact {rowid}.")
+        value = input(f"{info_type} : ")
+        Contact.update_database(rowid, info_type, value)
+
+        print(f"\nSuccessfully updated Contact {rowid}.")
+
+    else:
+        print(f"ID {rowid} does not exist.")
+
+
+def delete_contact_info(rowid):
+    # checks if contact exists first
+
+    if Contact.if_exists(rowid):
+        Contact.delete_from_database(rowid)
+    else:
+        print(f"ID {rowid} does not exist.")
 
 
 def main():
     import argparse
 
+    # making arguments for command
     parser = argparse.ArgumentParser(description="simple command to store contacts")
     parser.add_argument("--add", help="adds a contact", action="store_const", const="add")
     parser.add_argument("--list", help="lists all contacts", action="store_const", const="list")
@@ -143,7 +196,7 @@ def main():
     if args.list == "list":
         Contact.print()
     if args.delete is not None:
-        Contact.delete_from_database(args.delete)
+        delete_contact_info(args.delete)
     if args.update is not None:
         update_contact_info(args.update)
 
